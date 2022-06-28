1a47ad110b4d4b5c5e32f5469f8a7023091344fb
kevin-cueva <unimagneill@gmail.com>
2022-06-27 19:53:21 -0500

    post agregado

--- back/routes.py
+++ back/routes.py
@@ -1,5 +1,6 @@
-from flask_restful import Resource
-from flask import jsonify
+from ast import parse
+from flask_restful import Resource, reqparse
+from flask import jsonify, request
 from conexion import Conexion
 from conversiones import object_json
 
@@ -8,3 +9,7 @@ class Caballeros(Resource):
         detalles = Conexion.collection.find()
         data = object_json(detalles)
         return jsonify({"data": data}) 
+    def post(self):
+        data = request.json
+        Conexion.collection.insert_one(data)
+        return jsonify({"message":"successfully"})
